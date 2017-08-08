50baf8458edbab96fb4598588682b404975ede2c
iMac-52 <toek@iMac-52s-Mac-mini.local>
2017-08-08 11:02:21 +0700

    install pod file

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,14 +12,51 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLB: UILabel!
+    @IBOutlet weak var typeLB: UILabel!
+    @IBOutlet weak var defenseLB: UILabel!
+    @IBOutlet weak var heightLB: UILabel!
+    @IBOutlet weak var pokedexIdLB: UILabel!
+    @IBOutlet weak var weightLB: UILabel!
+    @IBOutlet weak var baseATKLb: UILabel!
+    @IBOutlet weak var evoLB: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    
     @IBOutlet weak var lbName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         lbName.text = pokemon.name
+        mainImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+        currentEvoImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        nextEvoImg.image = UIImage(named: "\(self.pokemon.pokedexId + 1)")
 
+    }
+    
+    
+    @IBAction func btnBack(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
     
     
 
 }
+
+
+
+
+
+
+
+
+
+
+
